0cf0cc2377617dda6c8083ffc3ba3ef99107a4c0
wahyuPM <wahyupratama191@gmail.com>
2022-12-06 13:20:34 +0800

    add : post preview component

--- pages/index.vue
+++ pages/index.vue
@@ -4,27 +4,23 @@
       <h1>Get the latest tech news</h1>
     </section>
     <section class="feutured-post">
-      <nuxt-link :to="'/posts/' + 1">
-        <article class="post-preview">
-          <div
-            class="post-thumbnail"
-            style="
-              background-image: url(https://wp.technologyreview.com/wp-content/uploads/2022/07/government-tech-insider.png);
-            "
-          ></div>
-          <div class="post-content">
-            <h1>Post title</h1>
-            <p>preview text</p>
-          </div>
-        </article>
-      </nuxt-link>
+      <PostPreview
+        id="1"
+        title="this is the title"
+        thumbnail="https://wp.technologyreview.com/wp-content/uploads/2022/07/government-tech-insider.png"
+        previewText="this is my first post!"
+      />
     </section>
   </div>
 </template>
 
 <script>
+import PostPreview from "@/components/Posts/PostPreview";
 export default {
   name: "IndexPage",
+  components: {
+    PostPreview,
+  },
 };
 </script>
 
@@ -66,41 +62,5 @@ export default {
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-}
-
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  background-color: white;
-  width: 90%;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-}
-
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-    margin: 10px;
-  }
-}
-
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-}
-
-.post-content {
-  padding: 10px;
-  text-align: center;
-}
-
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
 }
 </style>
