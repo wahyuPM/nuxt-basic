12d82c33adad8c9f321d0b5bcc560f654d17e818
wahyuPM <wahyupratama191@gmail.com>
2022-12-20 16:03:47 +0800

    add : middleware route

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -23,6 +23,7 @@ export default {
     }
   },
   layout: "admin",
+  middleware: "auth",
   components: {
     AdminPostForm,
   },
--- store/index.js
+++ store/index.js
@@ -91,6 +91,9 @@ const createStore = () => {
         getters: {
             loadedPosts(state) {
                 return state.loadedPosts
+            },
+            isAuthenticated(state){
+                return state.token != null;
             }
         }
     })
